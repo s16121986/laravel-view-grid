9a46f6738f1a6fb3f35788731cc862afff6858bf
Гурьянов Сергей <s16121986@yandex.ru>
2023-09-08 14:04:05 +0300

    chore: release 1.0.8

--- src/Column/ColumnInterface.php
+++ src/Column/ColumnInterface.php
@@ -2,6 +2,20 @@
 
 namespace Gsdk\Grid\Column;
 
-interface ColumnInterface {
+use Closure;
+
+/**
+ * @property string $name
+ * @property string $type
+ * @property string|null $text
+ * @property string|null $class
+ * @property string|null $route
+ * @property string|null $href
+ * @property string|null $emptyText
+ * @property bool $order
+ * @property Closure|null $renderer
+ */
+interface ColumnInterface
+{
 
 }
